057a46b6beb5da3fe0a30baeb732e451f3d8e20f
Suman <sumanpok9@gmail.com>
2023-08-18 01:49:03 +0545

    add jwt to sign token and check if a user is authorised to get access to resources

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,18 +1,23 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
 
 import pool from '../db/index';
+import { authenticateUser } from '../middleware/authJwt';
 const getUsers = async (req: Request, res: Response) => {
   const data = await pool.query('SELECT * FROM users');
   const { rows } = data;
 
   return res.status(200).json(rows);
 };
-const getUser = async (req: Request, res: Response) => {
-  const data = await pool.query(`SELECT * FROM users WHERE name = 'email'`);
+const getUser = async (req: Request, res: Response, next: NextFunction) => {
+  const data = await pool.query(`SELECT * FROM users WHERE fname = 'email'`);
   const { fields } = data;
-  console.log(fields);
-  return res.status(200).json(fields[0]);
+  // console.log(fields);
+  authenticateUser(req, res, next);
+  // return res.status(200).json(fields[0]);
 };
 
 const signupUser = async (req: Request, res: Response) => {
@@ -57,16 +62,27 @@ const signupUser = async (req: Request, res: Response) => {
 
 const loginUser = async (req: Request, res: Response) => {
   const { username, password } = req.body;
+  console.log(req.headers);
 
   if (username && password) {
-    const query = await pool.query('SELECT 1 fROM users WHERE fname = $1 ', [
+    const user = await pool.query('SELECT * fROM users WHERE fname = $1 ', [
       username,
     ]);
 
     //TODO authenticate if the hash password in the db is same as well
-    if (query.rows.length) {
+    if (user && user.rows.length) {
+      const secret = process.env.JWT_SECRET;
+      if (!secret) {
+        //ts sctrictly checks the type of the secret var .since it is being retrieved from env. it might not be defined . so make sure that secret is defined
+        throw new Error('No jwt key found');
+      }
+      const token = jwt.sign(user.rows[0].user_id, secret); //? add expiresTime
+      console.log(user.rows, token);
+      //? maybe set cookiw with the token as well instead of using the authorization headers
+
       res.status(200).json({
         message: 'successfully logged in', //set cookie and set header i guess
+        token,
       });
     } else {
       res.status(500).json({
---
+++ src/middleware/authJwt.ts
@@ -0,0 +1,22 @@
+import { Response, Request, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const authHeader = req.headers['authorization'];
+
+    const token = authHeader?.split(' ')[1]; //! ERRORS
+    console.log(token);
+    const secret = process.env.JWT_SECRET;
+    if (typeof token === 'string') {
+      if (!secret) throw new Error('secret key is undefined on authJwt.hs');
+      jwt.verify(token, secret);
+    }
+    next();
+  } catch (error) {
+    res.status(401).json({
+      errorMessage: 'UnAuthorised!!!',
+    });
+  }
+};
+
+export { authenticateUser };
